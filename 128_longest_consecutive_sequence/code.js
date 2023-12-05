3d118a24e511df886d48dffd9854f4922fc6e681
DustinYansberg <DustinYansberg@gmail.com>
2023-12-05 10:03:12 -0800

    added my solution too longestConsecutive

--- 128_longest_consecutive_sequence/code.js
+++ 128_longest_consecutive_sequence/code.js
@@ -1,25 +1,25 @@
-// https://leetcode.com/problems/longest-consecutive-sequence/
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-  console.log(nums);
-  nums = nums.sort((a, b) => a - b);
-  console.log(nums);
-  let currCount = 0;
-  let highestCount = 0;
-  for (let i = 1; i < nums.length; i++) {
-    nums[i] > nums[i - 1]
-      ? currCount++
-      : highestCount < currCount &&
-        ((highestCount = currCount), (currCount = 0));
-  }
-  return highestCount;
+  const set = new Set(nums);
+  let longestStreak = 0;
+  set.forEach((num) => {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (set.has(currentNum + 1)) {
+        currentStreak++;
+        currentNum++;
+      }
+      longestStreak = Math.max(longestStreak, currentStreak);
+    }
+  });
+  return longestStreak;
 };
 
-const nums = [100, 4, 200, 1, 3, 2];
+const nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
 // const nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
 
 console.log(longestConsecutive(nums));
