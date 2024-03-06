37a2bb31d7113d46e3b48915bc71c38fb8d26613
DustinYansberg <DustinYansberg@gmail.com>
2024-03-06 11:17:25 -0800

    minimum size subarray sum solution js

--- 209_minimum_size_subarray_sum/js/solution.js
+++ 209_minimum_size_subarray_sum/js/solution.js
@@ -1,27 +1,27 @@
 /**
  
-Given an array of positive integers nums and a positive integer target, return the minimal length of a
-subarray
-whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+*! Given an array of positive integers nums and a positive integer target, return the minimal length of a
+*! subarray
+*! whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
  
 
-Example 1:
-
-Input: target = 7, nums = [2,3,1,2,4,3]
-Output: 2
-
-Explanation: The subarray [4,3] has the minimal length under the problem constraint.
-
-Example 2:
-
-Input: target = 4, nums = [1,4,4]
-Output: 1
-
-Example 3:
-
-Input: target = 11, nums = [1,1,1,1,1,1,1,1]
-Output: 0
+** Example 1:
+** |
+** Input: target = 7, nums = [2,3,1,2,4,3]
+** Output: 2
+** 
+** Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+** 
+** Example 2:
+** 
+** Input: target = 4, nums = [1,4,4]
+** Output: 1
+** 
+** Example 3:
+** 
+** Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+** Output: 0
 
  */
 
@@ -31,30 +31,40 @@ Output: 0
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
-  if (nums.length === 0) {
-    return 0;
-  }
-
-  let minimumSubArray = Number.MAX_VALUE;
+  let minSize = Number.MAX_VALUE;
 
   let i = 0,
-    j = 0;
+    j = 1;
 
-  let runningTotal = nums[0];
+  let sum = nums[i];
 
-  while (i <= nums.length) {
-    if (runningTotal < target) {
-      if (j < nums.length - 1) {
-        j++;
-        runningTotal += nums[j];
-      } else {
-        break;
-      }
-    } else if (runningTotal >= target) {
-      minimumSubArray = Math.min(j + 1 - i, minimumSubArray);
-      runningTotal -= nums[i];
+  while (i < nums.length - 1) {
+    if (sum >= target) {
+      minSize = minSize > j - i + 1 ? j - i + 1 : minSize;
+      sum -= nums[i];
       i++;
+      continue;
     }
+    if (sum < target) {
+      j++;
+      sum += nums[j];
+      continue;
+    }
+    if (i === 0 || j >= nums.length - 1) break;
   }
-  return minimumSubArray == Number.MAX_VALUE ? 0 : minimumSubArray;
+
+  return minSize <= nums.length ? minSize : 0;
 };
+
+const target = 7,
+  nums = [2, 3, 1, 2, 4, 3];
+
+const target1 = 4,
+  nums1 = [1, 4, 4];
+
+const target2 = 15,
+  nums2 = [5, 1, 3, 5, 10, 7, 4, 9, 2, 8];
+
+console.log(minSubArrayLen(target, nums));
+console.log(minSubArrayLen(target1, nums1));
+console.log(minSubArrayLen(target2, nums2));
