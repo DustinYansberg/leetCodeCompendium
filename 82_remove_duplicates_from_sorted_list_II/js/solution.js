6778dec9b5737d7f55792365efd6cf9abd77665a
DustinYansberg <DustinYansberg@gmail.com>
2024-03-18 13:08:49 -0700

    modified solution in js

--- 82_remove_duplicates_from_sorted_list_II/js/solution.js
+++ 82_remove_duplicates_from_sorted_list_II/js/solution.js
@@ -69,59 +69,59 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-  //   if (!head) return null;
-  //   const arrMap = [[head.val, 1]];
+  if (!head) return null;
+  const arrMap = [[head.val, 1]];
 
-  //   let curr = head.next;
-  //   let it = 0;
+  let curr = head.next;
+  let it = 0;
 
-  //   while (curr) {
-  //     if (arrMap[it][0] === curr.val) {
-  //       arrMap[it][1]++;
-  //       curr = curr.next;
-  //       continue;
-  //     }
-
-  //     arrMap.push([curr.val, 1]);
-  //     it++;
-
-  //     curr = curr.next;
-  //   }
-  //   console.log(arrMap);
-  //   const newHead = new ListNode();
-  //   curr = newHead;
-  //   it = 0;
-
-  //   while (it < arrMap.length) {
-  //     if (arrMap[it][1] > 1) {
-  //       it++;
-  //       continue;
-  //     }
-  //     curr.val = arrMap[it][0];
-  //     it++;
-  //     if (it < arrMap.length) {
-  //       curr.next = new ListNode();
-  //       curr = curr.next;
-  //     }
-  //   }
-  //   head = newHead;
-  //   return head;
-
-  const sentinel = new ListNode(null, head);
-  let prev = sentinel;
-
-  while (head) {
-    if (head.next && head.val === head.next.val) {
-      while (head.next && head.val === head.next.val) {
-        head = head.next;
-      }
-      prev.next = head.next;
-    } else {
-      prev = prev.next;
+  while (curr) {
+    if (arrMap[it][0] === curr.val) {
+      arrMap[it][1]++;
+      curr = curr.next;
+      continue;
     }
-    head = head.next;
+
+    arrMap.push([curr.val, 1]);
+    it++;
+
+    curr = curr.next;
   }
-  return sentinel.next;
+  console.log(arrMap);
+  const newHead = new ListNode();
+  curr = newHead;
+  it = 0;
+
+  while (it < arrMap.length) {
+    if (arrMap[it][1] > 1) {
+      it++;
+      continue;
+    }
+    curr.val = arrMap[it][0];
+    it++;
+    if (it < arrMap.length) {
+      curr.next = new ListNode();
+      curr = curr.next;
+    }
+  }
+  head = newHead;
+  return head;
+
+  //   const sentinel = new ListNode(null, head);
+  //   let prev = sentinel;
+
+  //   while (head) {
+  //     if (head.next && head.val === head.next.val) {
+  //       while (head.next && head.val === head.next.val) {
+  //         head = head.next;
+  //       }
+  //       prev.next = head.next;
+  //     } else {
+  //       prev = prev.next;
+  //     }
+  //     head = head.next;
+  //   }
+  //   return sentinel.next;
 };
 
 let head = [1, 2, 3, 3, 4, 4, 5];
